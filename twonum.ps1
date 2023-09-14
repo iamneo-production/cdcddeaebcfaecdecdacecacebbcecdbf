aa07ca5995f5eaccbc0ea1e44045c653060d9cc4
Iamneo <techadmin@iamneo.ai>
2023-09-14 09:30:26 +0000

    Backup Commit

--- twonum.ps1
+++ twonum.ps1
@@ -1,6 +1,7 @@
-$x=Read-Host "Enter First Number"
-$y=Read-Host "Enter Second Number"
-if($( x + y) -gt 70){
+[int]$x=Read-Host "Enter First Number"
+[int]$y=Read-Host "Enter Second Number"
+# $sum = $x + $y
+if( ($x+$y) -gt 70){
     Write-Output "Pass"
 }
 else{
